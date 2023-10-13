a0fbf493600931c4f2717c467fcb189cd8f687b0
oozz00789 <93841207+oozz00789@users.noreply.github.com>
2023-10-13 14:55:39 +0800

    2-3-2.sql

---
+++ 2-3-2.sql
@@ -0,0 +1,20 @@
+USE [MySchoolDB]
+GO
+
+/****** Object:  Table [dbo].[Department]    Script Date: 2023/9/24 下午 05:25:21 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Department](
+	[Dep_code] [char](4) NOT NULL,
+	[Dep_name] [nvarchar](10) NOT NULL,
+	[Dep_head] [nvarchar](10) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Dep_code] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
